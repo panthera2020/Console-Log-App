5bcae4b5c5658635397b351951646f05ad7b1ff3
panthera2020 <diranoye2020@gmail.com>
2026-08-30 15:05:37 +0100

    Student grade book function compeleted and tested

---
+++ Student Grade Book/StudentGradeBookFunctions.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class StudentGradeBookFunctions {
+    private ArrayList<Integer> scores = new ArrayList<>();
+    
+    public  ArrayList<Integer> checkScores(){
+        arrangeScores();
+        return scores;
+    }
+    
+    public void addScore(int score){
+        if(score >= 0 && score <= 100){
+            scores.add(score);
+        }
+    }
+    
+    public void arrangeScores(){
+        int store = 0;
+        for(int index = 0; index < scores.size(); index++){
+            for(int count = 0; count < scores.size(); count++){
+                if(scores.get(index) < scores.get(count)){
+                    store = scores.get(index);
+                    scores.set(index, scores.get(count));
+                    scores.set(count, store);
+                }
+            }
+        }
+    }
+    
+    public boolean isScoreArrayEmpty(){
+        boolean isEmpty = false;
+        if(scores.size() == 0){
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
+    
+    public int getHighestScore(){
+        arrangeScores();
+        int highestScore = scores.get((scores.size() - 1));
+        return highestScore;
+    }
+    
+    public int getLowestScore(){
+        arrangeScores();
+        int lowestScore = scores.get(0);
+        return lowestScore;
+    }
+    
+    public int getAverage(){
+        int sum = 0;
+        for(int index = 0; index < scores.size(); index++){
+            sum += scores.get(index);
+        }
+        
+        int average = sum / scores.size();
+        
+        return average;
+    }
+    
+    public int scoresAboveAverage(){
+        int counter = 0;
+        for(int index = 0; index < scores.size(); index++){
+            if(scores.get(index) >= 50){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ Student Grade Book/TestStudentGradeBookFunctions.java
@@ -0,0 +1,185 @@
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestStudentGradeBookFunctions {
+    private StudentGradeBookFunctions gradeBook;
+
+    @BeforeEach
+    void createObject(){
+        gradeBook = new StudentGradeBookFunctions();
+    }
+    
+    @Test
+    void testThatWhenCheckScoreArrayItIsEmpty(){
+        ArrayList <Integer> scores = gradeBook.checkScores();
+        assertEquals(0,scores.size());
+    }
+    
+    @Test
+    void testThatWhenIAddScoreItIsAddedToTheArray(){
+        //Given
+        int score = 50;
+        
+        //When
+        gradeBook.addScore(score);
+        ArrayList <Integer> scores = gradeBook.checkScores();
+        
+        //Check
+        assertEquals(1,scores.size());        
+    }
+    
+    @Test
+    void testThatWhenIAddANegativeNumberItDoesNotAddToTheScores(){
+        //Given
+        int score = -1;
+        
+        //When
+        gradeBook.addScore(score);
+        ArrayList <Integer> scores = gradeBook.checkScores();
+        
+        //Check
+        assertEquals(0,scores.size()); 
+    }
+    
+    @Test
+    void testThatWhenIAddAScoreGreaterThanHunderdItDoNotAddToArray(){
+        //Given
+        int score = 200;
+        
+        //When
+        gradeBook.addScore(score);
+        ArrayList <Integer> scores = gradeBook.checkScores();
+        
+        //Check
+        assertEquals(0,scores.size());
+    }
+    
+    @Test
+    void testThatWhenICheckScoreItIsSortedAndArranged(){
+        //Given
+        int firstScore = 20;
+        int secondScore = 90;
+        int thirdScore = 55;
+        
+        //When
+        gradeBook.addScore(firstScore);
+        gradeBook.addScore(secondScore);
+        gradeBook.addScore(thirdScore);
+        
+        ArrayList<Integer> actualScores = gradeBook.checkScores();
+        ArrayList<Integer> expectedScores = new ArrayList<>();
+        expectedScores.add(20);
+        expectedScores.add(55);
+        expectedScores.add(90);
+        
+        //Check
+        assertEquals(expectedScores, actualScores);
+    }
+    
+    @Test
+    void testThatWhenICheckForTheHighestScoreIGetTheHighestScore(){
+        //Given
+        int firstScore = 20;
+        int secondScore = 90;
+        int thirdScore = 55;
+        
+        //When
+        gradeBook.addScore(firstScore);
+        gradeBook.addScore(secondScore);
+        gradeBook.addScore(thirdScore);
+        
+        int actualHighestScore = gradeBook.getHighestScore();
+        int expectedHighestScore = 90;
+        
+        //Check
+        assertEquals(expectedHighestScore, actualHighestScore);
+    }
+    
+    @Test
+    void testThatWhenICheckForLowestScoreIGetTheLowestScore(){
+        //Given
+        int firstScore = 20;
+        int secondScore = 90;
+        int thirdScore = 55;
+        
+        //When
+        gradeBook.addScore(firstScore);
+        gradeBook.addScore(secondScore);
+        gradeBook.addScore(thirdScore);
+        
+        int actualLowestScore = gradeBook.getLowestScore();
+        int expectedLowestScore = 20;
+        
+        //Check
+        assertEquals(expectedLowestScore, actualLowestScore);
+    }
+    
+    @Test
+    void testThatWHenTheScoreArrayIsEmptyItReturnsTrueWhenNoScoreHasBeenAdded(){
+        boolean actualAnswer = gradeBook.isScoreArrayEmpty();
+        boolean expectedAnswer = true;
+        
+        //Check
+        assertEquals(expectedAnswer,actualAnswer);
+    }
+    
+    @Test
+    void testThatWhenIAskForAverageIGetTheAverageOfAllTheScores(){
+        //Given
+        int firstScore = 20;
+        int secondScore = 90;
+        int thirdScore = 40;
+        
+        //When
+        gradeBook.addScore(firstScore);
+        gradeBook.addScore(secondScore);
+        gradeBook.addScore(thirdScore);
+        
+        int actualAverage = gradeBook.getAverage();
+        int expectedAverage = 50;
+        
+        //Check
+        assertEquals(expectedAverage,actualAverage);
+        
+    }
+    
+    @Test
+    void testThatWhenIAsForScoresAboveFiftyItReturnsTheNumberOfScoresAboveFifty(){
+        //Given
+        int firstScore = 20;
+        int secondScore = 90;
+        int thirdScore = 40;
+        int fourthScore = 55;
+        
+        //When
+        gradeBook.addScore(firstScore);
+        gradeBook.addScore(secondScore);
+        gradeBook.addScore(thirdScore);
+        gradeBook.addScore(fourthScore);
+        
+        int actualAnswer = gradeBook.scoresAboveAverage();
+        int expectedAnswer = 2;
+        
+        //Check
+        assertEquals(expectedAnswer,actualAnswer);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
